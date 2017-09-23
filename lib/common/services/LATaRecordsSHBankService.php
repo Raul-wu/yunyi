7c5cbcbcf6e1099285b68aa6cba8861d6f8c2bd9
Raul Wu <raul.wu@blackboard.com>
2017-09-23 21:20:57 +0800

    fix bug

--- lib/common/services/LATaRecordsSHBankService.php
+++ lib/common/services/LATaRecordsSHBankService.php
@@ -64,7 +64,7 @@ class LATaRecordsSHBankService
 
         $objPhpExcel->getProperties()->setCreator("TA")
             ->setLastModifiedBy("TA")
-            ->setTitle('yunyi')
+            ->setTitle('yunyin$objPhpExcel->getActiveSheet()->getColumnDimension')
             ->setSubject("")
             ->setDescription("")
             ->setKeywords("")
@@ -75,22 +75,23 @@ class LATaRecordsSHBankService
         $objAlignA1  = $objStyleA1->getAlignment();
         $objAlignA1->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 
-        $objActSheet->getColumnDimension('A')->setWidth(30);
-        $objActSheet->getColumnDimension('B')->setWidth(30);
-        $objActSheet->getColumnDimension('C')->setWidth(30);
-        $objActSheet->getColumnDimension('D')->setWidth(30);
-        $objActSheet->getColumnDimension('E')->setWidth(30);
-        $objActSheet->getColumnDimension('F')->setWidth(30);
-        $objActSheet->getColumnDimension('G')->setWidth(30);
-        $objActSheet->getColumnDimension('H')->setWidth(30);
-        $objActSheet->getColumnDimension('I')->setWidth(30);
-        $objActSheet->getColumnDimension('J')->setWidth(30);
-        $objActSheet->getColumnDimension('K')->setWidth(30);
-        $objActSheet->getColumnDimension('L')->setWidth(30);
-        $objActSheet->getColumnDimension('M')->setWidth(30);
-        $objActSheet->getColumnDimension('N')->setWidth(30);
-        $objActSheet->getColumnDimension('O')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('A')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('B')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('C')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('D')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('E')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('F')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('G')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('H')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('I')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('J')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('K')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('L')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('M')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('N')->setWidth(30);
+        $objPhpExcel->getActiveSheet()->getColumnDimension('O')->setWidth(30);
 
+        $objPhpExcel->getActiveSheet()->setTitle('上海银行版');
         $objPhpExcel->getActiveSheet()->setCellValue('A1', '投资者名称');
         $objPhpExcel->getActiveSheet()->setCellValue('B1', '投资者类型');
         $objPhpExcel->getActiveSheet()->setCellValue('C1', '证件类型');
@@ -98,7 +99,7 @@ class LATaRecordsSHBankService
         $objPhpExcel->getActiveSheet()->setCellValue('E1', '投资者账号');
         $objPhpExcel->getActiveSheet()->setCellValue('F1', '开户行名称');
         $objPhpExcel->getActiveSheet()->setCellValue('G1', '类别');
-        $objPhpExcel->getActiveSheet()->setCellValue('H1', '持有份额');
+        $objPhpExcel->getActiveSheet()->setCellValue('H1', '持有份额(份)');
         $objPhpExcel->getActiveSheet()->setCellValue('I1', '份额确认日');
         $objPhpExcel->getActiveSheet()->setCellValue('J1', '起息日');
         $objPhpExcel->getActiveSheet()->setCellValue('K1', '截止日');
@@ -110,21 +111,21 @@ class LATaRecordsSHBankService
         foreach($shanghaiBank as $k => $v )
         {
             $i++;
-            $objPhpExcel->getActiveSheet()->setCellValue('A' . $i, isset($v['name']) ? $v['name'] : '');
-            $objPhpExcel->getActiveSheet()->setCellValue('B' . $i, isset($v['type']) ? $v['type'] : '');
-            $objPhpExcel->getActiveSheet()->setCellValue('C' . $i, isset($v['id_type']) ? $v['id_type'] : '');
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('A' . $i, isset($v['name']) ? $v['name'] : '', PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('B' . $i, isset($v['type']) ? $v['type'] : '', PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('C' . $i, isset($v['id_type']) ? $v['id_type'] : '', PHPExcel_Cell_DataType::TYPE_STRING);
             $objPhpExcel->getActiveSheet()->setCellValueExplicit('D' . $i, isset($v['id_content']) ? $v['id_content'] : '', PHPExcel_Cell_DataType::TYPE_STRING);
             $objPhpExcel->getActiveSheet()->setCellValueExplicit('E' . $i, isset($v['bank_account']) ? $v['bank_account'] : '', PHPExcel_Cell_DataType::TYPE_STRING);
-            $objPhpExcel->getActiveSheet()->setCellValue('F' . $i, isset($v['bank_address']) ? $v['bank_address'] : '');
-//            $objPhpExcel->getActiveSheet()->setCellValue('G' . $i, $v['']);
-            $objPhpExcel->getActiveSheet()->setCellValue('H' . $i, isset($v['amount']) ? $v['amount'] : '');
-            $objPhpExcel->getActiveSheet()->setCellValue('I' . $i, isset($v['conformation_date']) ? $v['conformation_date'] : '');
-            $objPhpExcel->getActiveSheet()->setCellValue('J' . $i, isset($v['value_date']) ? $v['value_date'] : '');
-            $objPhpExcel->getActiveSheet()->setCellValue('K' . $i, isset($v['expected_date']) ? $v['expected_date'] : '');
-            $objPhpExcel->getActiveSheet()->setCellValue('I' . $i, ($v['expected_date'] - $v['value_date']) / 86400);
-            $objPhpExcel->getActiveSheet()->setCellValue('M' . $i, isset($v['income_rate_E6']) ? $v['income_rate_E6'] : '');
-            $objPhpExcel->getActiveSheet()->setCellValue('N' . $i, $v['total'] - $v['amount']);
-            $objPhpExcel->getActiveSheet()->setCellValue('O' . $i, isset($v['total']) ? $v['total'] : '');
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('F' . $i, isset($v['bank_address']) ? $v['bank_address'] : '', PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('G' . $i, '', PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('H' . $i, isset($v['amount']) ? $v['amount'] * LConstService::E4 : '', PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('I' . $i, isset($v['conformation_date']) ? date('Y-m-d', strtotime($v['conformation_date'])) : '', PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('J' . $i, isset($v['value_date']) ? $v['value_date'] : '', PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('K' . $i, isset($v['expected_date']) ? $v['expected_date'] : '', PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('L' . $i, (strtotime($v['expected_date']) - strtotime($v['value_date'])) / 86400, PHPExcel_Cell_DataType::TYPE_STRING) ;
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('M' . $i, isset($v['income_rate_E6']) ? $v['income_rate_E6'] : '', PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('N' . $i, $v['total'], PHPExcel_Cell_DataType::TYPE_STRING);
+            $objPhpExcel->getActiveSheet()->setCellValueExplicit('O' . $i, $v['total'] + $v['amount'], PHPExcel_Cell_DataType::TYPE_STRING);
         }
 
         $objWriter = PHPExcel_IOFactory::createWriter($objPhpExcel, 'Excel2007');
