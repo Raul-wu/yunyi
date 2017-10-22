ca1183264b5f92ed1acbbcf054cc2ed6f8178b33
Raul Wu <raul.wu@blackboard.com>
2017-10-22 14:43:18 +0800

    ta bug fix

--- lib/common/services/LATaService.php
+++ lib/common/services/LATaService.php
@@ -184,7 +184,7 @@ class LATaService
                 'amount' => $arrQuotient->amount,
                 'conformation_date' => $arrQuotient->create_time,
                 'value_date' => date('Y-m-d', $arrQuotient->buy_date),
-                'expected_date' => $objPProduct->type == LAPProductModel::TYPE_FI ? date('Y-m-d', $objPProduct->expected_date) : date('Y-m-d', $objTa->fact_end_date),
+                'expected_date' => date('Y-m-d', $objTa->fact_end_date),
                 'income_rate_E6' => $objTa->fact_income_rate_E6 / LConstService::E4 ,
                 'total' => $total
             );
@@ -207,7 +207,7 @@ class LATaService
                 'conformation_quotient' => $arrQuotient->amount,
                 'has_quotient' => $arrQuotient->amount,
                 'value_date' => date('Y-m-d', $arrQuotient->buy_date),
-                'expected_date' => $objPProduct->type == LAPProductModel::TYPE_FI ? date('Y-m-d', $objPProduct->expected_date) : date('Y-m-d', $objTa->fact_end_date),
+                'expected_date' => date('Y-m-d', $objTa->fact_end_date),
                 'income_rate_E6' => $objTa->fact_income_rate_E6 / LConstService::E4 ,
                 'total' => $total
             );
